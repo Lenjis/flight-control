0b847880a32ee1092b7488c25941efc0f389add6
Lenjis <rzxhandsome@sina.com>
2025-04-20 23:04:46 +0800

    edit visualize.m

--- sim-c-visualize/load_data.m
+++
@@ -1,7 +0,0 @@
-filePath = 'simu.txt';
-
-% 读取数据
-cout = readtable(filePath, 'Delimiter', '\t', 'ReadVariableNames', true);
-
-% 检查数据
-disp('数据已成功加载.');
--- sim-c-visualize/visualize.m
+++ sim-c-visualize/visualize.m
@@ -1,6 +1,17 @@
-load_data;
-close all;
-plot(cout.t, cout.H, 'LineWidth', 2);
+filePath = 'simu.txt';
 
+% read the data
+cout = readtable(filePath, 'Delimiter', '\t', 'ReadVariableNames', true);
+
+info = stepinfo(cout.theta - 1.1190407073, cout.t);
+disp(info);
+
+% plot c simulation output
+close all;
+plot(cout.t, cout.theta - 1.1190407073, 'LineWidth', 2);
+ylim([-1 2]);
+xlim([0 5]);
+
+% plot simulink simulation output
 hold on
-plot(simout.H.Time, simout.H.Data, 'LineWidth', 2);
+% plot(simout.H.Time, simout.H.Data, 'LineWidth', 2);
